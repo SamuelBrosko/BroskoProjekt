975a48b88894713c6d70b38da86a88725274c4d6
SamuelBrosko <broskosamuel13@gmail.com>
2025-03-27 15:10:56 +0100

    init

---
+++ header.php
@@ -0,0 +1,9 @@
+<header class="header-area header-sticky">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <?php include 'nav.php'; ?>
+            </div>
+        </div>
+    </div>
+</header>
