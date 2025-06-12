dda6d12aad5559bff9c6be2f235349fef149e53b
SamuelBrosko <broskosamuel13@gmail.com>
2025-06-12 20:35:05 +0200

    browse tweak 1

--- browse.php
+++ browse.php
@@ -44,43 +44,9 @@ https://templatemo.com/tm-579-cyborg-gaming
     </div>
   </div>
   <!-- ***** Preloader End ***** -->
-
+  <?php include 'header.php'; ?>
   <!-- ***** Header Area Start ***** -->
-  <header class="header-area header-sticky">
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <nav class="main-nav">
-                    <!-- ***** Logo Start ***** -->
-                    <a href="index.php" class="logo">
-                        <img src="assets/images/logo.png" alt="">
-                    </a>
-                    <!-- ***** Logo End ***** -->
-                    <!-- ***** Search End ***** -->
-                    <div class="search-input">
-                      <form id="search" action="#">
-                        <input type="text" placeholder="Type Something" id='searchText' name="searchKeyword" onkeypress="handle" />
-                        <i class="fa fa-search"></i>
-                      </form>
-                    </div>
-                    <!-- ***** Search End ***** -->
-                    <!-- ***** Menu Start ***** -->
-                    <ul class="nav">
-                        <li><a href="index.php">Home</a></li>
-                        <li><a href="browse.html" class="active">Browse</a></li>
-                        <li><a href="details.html">Details</a></li>
-                        <li><a href="streams.html">Streams</a></li>
-                        <li><a href="profile.html">Profile <img src="assets/images/profile-header.jpg" alt=""></a></li>
-                    </ul>   
-                    <a class='menu-trigger'>
-                        <span>Menu</span>
-                    </a>
-                    <!-- ***** Menu End ***** -->
-                </nav>
-            </div>
-        </div>
-    </div>
-  </header>
+
   <!-- ***** Header Area End ***** -->
 
   <div class="container">
@@ -98,12 +64,12 @@ https://templatemo.com/tm-579-cyborg-gaming
                 <div class="owl-features owl-carousel">
                   <div class="item">
                     <div class="thumb">
-                      <img src="assets/images/featured-01.jpg" alt="">
+                      <img src="assets/images/moto1.jpg" alt="">
                       <div class="hover-effect">
                         <h6>2.4K Streaming</h6>
                       </div>
                     </div>
-                    <h4>CS-GO<br><span>249K Downloads</span></h4>
+                    <h4>KTM 600<br><span>5000$</span></h4>
                     <ul>
                       <li><i class="fa fa-star"></i> 4.8</li>
                       <li><i class="fa fa-download"></i> 2.3M</li>
@@ -111,12 +77,12 @@ https://templatemo.com/tm-579-cyborg-gaming
                   </div>
                   <div class="item">
                     <div class="thumb">
-                      <img src="assets/images/featured-02.jpg" alt="">
+                      <img src="assets/images/moto2.jpg" alt="">
                       <div class="hover-effect">
                         <h6>2.4K Streaming</h6>
                       </div>
                     </div>
-                    <h4>Gamezer<br><span>249K Downloads</span></h4>
+                    <h4>Harley<br><span>8000$</span></h4>
                     <ul>
                       <li><i class="fa fa-star"></i> 4.8</li>
                       <li><i class="fa fa-download"></i> 2.3M</li>
@@ -124,12 +90,12 @@ https://templatemo.com/tm-579-cyborg-gaming
                   </div>
                   <div class="item">
                     <div class="thumb">
-                      <img src="assets/images/featured-03.jpg" alt="">
+                      <img src="assets/images/moto3.jpg" alt="">
                       <div class="hover-effect">
                         <h6>2.4K Streaming</h6>
                       </div>
                     </div>
-                    <h4>Island Rusty<br><span>249K Downloads</span></h4>
+                    <h4>Honda<br><span>7000$</span></h4>
                     <ul>
                       <li><i class="fa fa-star"></i> 4.8</li>
                       <li><i class="fa fa-download"></i> 2.3M</li>
@@ -137,12 +103,12 @@ https://templatemo.com/tm-579-cyborg-gaming
                   </div>
                   <div class="item">
                     <div class="thumb">
-                      <img src="assets/images/featured-01.jpg" alt="">
+                      <img src="assets/images/moto4.jpg" alt="">
                       <div class="hover-effect">
                         <h6>2.4K Streaming</h6>
                       </div>
                     </div>
-                    <h4>CS-GO<br><span>249K Downloads</span></h4>
+                    <h4>Harley<br><span>2900$</span></h4>
                     <ul>
                       <li><i class="fa fa-star"></i> 4.8</li>
                       <li><i class="fa fa-download"></i> 2.3M</li>
@@ -150,12 +116,12 @@ https://templatemo.com/tm-579-cyborg-gaming
                   </div>
                   <div class="item">
                     <div class="thumb">
-                      <img src="assets/images/featured-02.jpg" alt="">
+                      <img src="assets/images/moto5.jpg" alt="">
                       <div class="hover-effect">
                         <h6>2.4K Streaming</h6>
                       </div>
                     </div>
-                    <h4>Gamezer<br><span>249K Downloads</span></h4>
+                    <h4>Suzuki<br><span>3999$</span></h4>
                     <ul>
                       <li><i class="fa fa-star"></i> 4.8</li>
                       <li><i class="fa fa-download"></i> 2.3M</li>
@@ -163,12 +129,12 @@ https://templatemo.com/tm-579-cyborg-gaming
                   </div>
                   <div class="item">
                     <div class="thumb">
-                      <img src="assets/images/featured-03.jpg" alt="">
+                      <img src="assets/images/moto6.jpg" alt="">
                       <div class="hover-effect">
                         <h6>2.4K Streaming</h6>
                       </div>
                     </div>
-                    <h4>Island Rusty<br><span>249K Downloads</span></h4>
+                    <h4>Kawasaki<br><span>6000$</span></h4>
                     <ul>
                       <li><i class="fa fa-star"></i> 4.8</li>
                       <li><i class="fa fa-download"></i> 2.3M</li>
@@ -180,13 +146,13 @@ https://templatemo.com/tm-579-cyborg-gaming
             <div class="col-lg-4">
               <div class="top-downloaded">
                 <div class="heading-section">
-                  <h4><em>Top</em> Downloaded</h4>
+                  <h4><em>Top</em> Sellers</h4>
                 </div>
                 <ul>
                   <li>
-                    <img src="assets/images/game-01.jpg" alt="" class="templatemo-item">
-                    <h4>Fortnite</h4>
-                    <h6>Sandbox</h6>
+                    <img src="assets/images/moto2.jpg" alt="" class="templatemo-item">
+                    <h4>Yamaha</h4>
+                    <h6>Comfort</h6>
                     <span><i class="fa fa-star" style="color: yellow;"></i> 4.9</span>
                     <span><i class="fa fa-download" style="color: #ec6090;"></i> 2.2M</span>
                     <div class="download">
@@ -194,9 +160,9 @@ https://templatemo.com/tm-579-cyborg-gaming
                     </div>
                   </li>
                   <li>
-                    <img src="assets/images/game-02.jpg" alt="" class="templatemo-item">
-                    <h4>CS-GO</h4>
-                    <h6>Legendary</h6>
+                    <img src="assets/images/moto8.jpg" alt="" class="templatemo-item">
+                    <h4>Kawasaki</h4>
+                    <h6>Speed</h6>
                     <span><i class="fa fa-star" style="color: yellow;"></i> 4.9</span>
                     <span><i class="fa fa-download" style="color: #ec6090;"></i> 2.2M</span>
                     <div class="download">
@@ -204,9 +170,9 @@ https://templatemo.com/tm-579-cyborg-gaming
                     </div>
                   </li>
                   <li>
-                    <img src="assets/images/game-03.jpg" alt="" class="templatemo-item">
-                    <h4>PugG</h4>
-                    <h6>Battle Royale</h6>
+                    <img src="assets/images/moto4.jpg" alt="" class="templatemo-item">
+                    <h4>Honda</h4>
+                    <h6>Cruise</h6>
                     <span><i class="fa fa-star" style="color: yellow;"></i> 4.9</span>
                     <span><i class="fa fa-download" style="color: #ec6090;"></i> 2.2M</span>
                     <div class="download">
@@ -215,7 +181,7 @@ https://templatemo.com/tm-579-cyborg-gaming
                   </li>
                 </ul>
                 <div class="text-button">
-                  <a href="profile.html">View All Games</a>
+                  <a href="profile.html">View All Motorbikes</a>
                 </div>
               </div>
             </div>
@@ -226,7 +192,7 @@ https://templatemo.com/tm-579-cyborg-gaming
           <div class="start-stream">
             <div class="col-lg-12">
               <div class="heading-section">
-                <h4><em>How To Start Your</em> Live Stream</h4>
+                <h4><em>How To Start</em> Riding</h4>
               </div>
               <div class="row">
                 <div class="col-lg-4">
@@ -387,18 +353,8 @@ https://templatemo.com/tm-579-cyborg-gaming
       </div>
     </div>
   </div>
-  
-  <footer>
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-12">
-          <p>Copyright © 2036 <a href="#">Cyborg Gaming</a> Company. All rights reserved. 
-          
-          <br>Design: <a href="https://templatemo.com" target="_blank" title="free CSS templates">TemplateMo</a></p>
-        </div>
-      </div>
-    </div>
-  </footer>
+
+  <?php include 'footer.php'; ?>
 
 
   <!-- Scripts -->
